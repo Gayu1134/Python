c8c01de19d8d5083988b28aad57fb9b5dc2aeef7
manimike00 <manimike00@gmail.com>
2018-12-16 10:40:13 +0530

    Amstrong number

---
+++ amstrong.py
@@ -0,0 +1,21 @@
+number = int(input("Enter the number : "))
+n1 = number
+len = 0
+rem = 0
+sum = 1
+temp = 0
+while(n1!=0):
+	len+=1
+	n1=n1//10
+n2 = number
+while(n2!=0):
+	rem = n2 % 10
+	for i in range(len):
+		sum *= rem
+	temp += sum
+	sum = 1
+	n2 = n2 // 10
+if (number == temp):
+	print("yes")
+else:
+	print("No") 
