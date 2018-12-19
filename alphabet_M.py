7b0874e2c3e2ccbbd49254a0376e985096e0dab3
manimike00 <manimike00@gmail.com>
2018-12-19 20:35:48 +0530

    Alphabet M

---
+++ alphabet_M.py
@@ -0,0 +1,19 @@
+for i in range(5):
+	for j in range(5):
+		if(i==0 and j!=0 and j!=4):
+			print(" ",end=" ")
+		else:
+			if(i==1 and j==2):
+				print(" ",end=" ")
+			else:
+				if(i==2 and j!=0 and j!=2 and j!=4):
+					print(" ",end=" ")
+				else:
+					if(i==3 and j!=0 and j!=4):
+						print(" ",end=" ")
+					else:
+						if(i==4 and j!=0 and j!=4):
+							print(" ",end=" ")
+						else:
+							print("+",end=" ")
+	print()
