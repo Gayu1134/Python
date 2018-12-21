59df275321547ece83e57e425e3627af5ccd79d7
manimike00 <manimike00@gmail.com>
2018-12-21 17:11:20 +0530

    M is changed

--- alphabet_M.py
+++ alphabet_M.py
@@ -12,7 +12,7 @@ for i in range(5):
 					if(i==3 and j!=0 and j!=4):
 						print(" ",end=" ")
 					else:
-						if(i==4 and j!=0 and j!=4):
+						if(i==4 and j!=0):
 							print(" ",end=" ")
 						else:
 							print("+",end=" ")
